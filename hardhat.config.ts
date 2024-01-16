b7cfd61da9169ba4a0ecaf3d78e3dbc492ee3017
nmempiric99 <Neha.m@empiricinfotech.com>
2024-01-16 14:23:03 +0530

    some changes in hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -54,30 +54,6 @@ export default {
     mainnet: {
       url: `https://eth-mainnet.g.alchemy.com/v2/1RegSfonVLXiWsj6j20_EkKiylU5GEP5`,
     },
-    // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // goerli: {
-    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // kovan: {
-    //   url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrumRinkeby: {
-    //   url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrum: {
-    //   url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimismKovan: {
-    //   url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimism: {
-    //   url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
     optimismsepolia:{
       url:`https://sepolia.optimism.io`
     },
