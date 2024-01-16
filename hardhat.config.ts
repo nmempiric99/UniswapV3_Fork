75328cb54c97cf9ea47019ce1fe06554f87e8992
nmempiric99 <Neha.m@empiricinfotech.com>
2024-01-16 10:45:11 +0530

    router

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,37 +25,53 @@ export default {
       allowUnlimitedContractSize: false,
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://eth-mainnet.g.alchemy.com/v2/1RegSfonVLXiWsj6j20_EkKiylU5GEP5`,
     },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    arbitrumRinkeby: {
-      url: `https://rinkeby.arbitrum.io/rpc`,
-    },
-    arbitrum: {
-      url: `https://arb1.arbitrum.io/rpc`,
-    },
-    optimismKovan: {
-      url: `https://kovan.optimism.io`,
-    },
-    optimism: {
-      url: `https://mainnet.optimism.io`,
-    },
+    // ropsten: {
+    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // rinkeby: {
+    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // goerli: {
+    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // kovan: {
+    //   url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // arbitrumRinkeby: {
+    //   url: `https://rinkeby.arbitrum.io/rpc`,
+    // },
+    // arbitrum: {
+    //   url: `https://arb1.arbitrum.io/rpc`,
+    // },
+    // optimismKovan: {
+    //   url: `https://kovan.optimism.io`,
+    // },
+    // optimism: {
+    //   url: `https://mainnet.optimism.io`,
+    // },
+    optimismsepolia:{
+      // url: `https://opt-sepolia.g.alchemy.com/v2/UNJsvPczvsttyxlW2243dXYDFzLDIIAs`,
+      url:`https://sepolia.optimism.io`
+    }
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      'optimismsepolia': 'RGE3WPFI978V7GKF11AV2BG62TTJZ1DQ14',
+    },
+    customChains: [
+      {
+        network: 'optimismsepolia',
+        chainId: 11155420,
+        urls: {
+          apiURL: 'https://api-sepolia-optimism.etherscan.io/api',
+          browserURL: 'https://sepolia-optimism.etherscan.io/',
+        },
+      },
+    ]
   },
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
