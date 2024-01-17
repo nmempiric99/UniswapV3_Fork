05484098b6f45caa387f998c5511382d45f5926d
nmempiric99 <Neha.m@empiricinfotech.com>
2024-01-17 11:43:10 +0530

    changes in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,6 @@ import '@nomiclabs/hardhat-etherscan'
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("infura key---------",process.env.INFURA_API_KEY);
 
 export default {
   networks: {
@@ -15,57 +14,26 @@ export default {
     mainnet: {
       url: `https://eth-mainnet.g.alchemy.com/v2/1RegSfonVLXiWsj6j20_EkKiylU5GEP5`,
     },
-    // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // goerli: {
-    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // kovan: {
-    //   url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrumRinkeby: {
-    //   url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrum: {
-    //   url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimismKovan: {
-    //   url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimism: {
-    //   url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // mumbai: {
-    //   url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // polygon: {
-    //   url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // bnb: {
-    //   url: `https://bsc-dataseed.binance.org/`,
-    // },
     optimismsepolia:{
-      // url: `https://opt-sepolia.g.alchemy.com/v2/UNJsvPczvsttyxlW2243dXYDFzLDIIAs`,
       url:`https://sepolia.optimism.io`
+    },
+    vault:{
+      url:`https://rpc.vault.tech/`
     }
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: {
-      'optimismsepolia': 'RGE3WPFI978V7GKF11AV2BG62TTJZ1DQ14',
+      'vault': 'eded7506-000c-48d4-8455-bc575c66ec84',
     },
     customChains: [
       {
-        network: 'optimismsepolia',
-        chainId: 11155420,
+        network: 'vault',
+        chainId: 79000,
         urls: {
-          apiURL: 'https://api-sepolia-optimism.etherscan.io/api',
-          browserURL: 'https://sepolia-optimism.etherscan.io/',
+          apiURL: 'https://blockscout.com/eth/mainnet/api/eth-rpc',
+          browserURL: 'https://rpc.vault.tech/',
         },
       },
     ]
